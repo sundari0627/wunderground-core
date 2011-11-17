e8f698d64d4d719c1eed3151cf36823cbe5eb748
benning.martin@gmail.com <benning.martin@gmail.com@ec73e691-2b8d-3a17-928f-8130a06589c6>
2011-11-17 17:22:37 +0000

    implement WeatherStationService

---
+++ trunk/src/test/java/de/mbenning/weather/wunderground/services/base/AbstractBaseServiceTest.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package de.mbenning.weather.wunderground.services.base;
+
+import java.text.SimpleDateFormat;
+
+import de.mbenning.weather.wunderground.api.domain.DataSet;
+import de.mbenning.weather.wunderground.api.domain.WeatherStation;
+
+/**
+ * @author Martin.Benning
+ *
+ */
+public abstract class AbstractBaseServiceTest {
+	
+	protected SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+
+	protected void printWeatherStationData(WeatherStation weatherStation, DataSet dataSet) {
+		String city 	= weatherStation.getCity() != null ? weatherStation.getCity() : "";
+		String state 	= weatherStation.getState() != null ? weatherStation.getState() : "";
+		String country 	= weatherStation.getCountry() != null ? weatherStation.getCountry() : "";
+		
+		System.out.println(weatherStation.getStationId() + " " + city + " " + state + " " + country);
+		
+		System.out.println("DATETIME\t TEMP\tDEW\tWSPD");
+		System.out.println(sdf.format(dataSet.getDateTime()) + " " + dataSet.getTemperature() + "\t" + dataSet.getDewPoint() + "\t" + dataSet.getWindSpeedKmh());
+		
+		System.out.println("");
+	}
+	
+}
