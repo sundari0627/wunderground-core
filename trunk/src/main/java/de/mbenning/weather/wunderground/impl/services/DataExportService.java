78422b34b99f1a35084f0633369a761a09ce247f
benning.martin@googlemail.com <benning.martin@googlemail.com@ec73e691-2b8d-3a17-928f-8130a06589c6>
2012-07-20 10:59:38 +0000

    + CSV Data Service

---
+++ trunk/src/main/java/de/mbenning/weather/wunderground/impl/services/DataExportService.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package de.mbenning.weather.wunderground.impl.services;
+
+import de.mbenning.weather.wunderground.api.domain.IDataExport;
+import de.mbenning.weather.wunderground.api.services.IExportService;
+
+/**
+ * @author Martin.Benning
+ *
+ */
+public class DataExportService implements IExportService {
+
+	public void process(IDataExport dataExport) {
+	}
+	
+}
